9b056177492bff1604b066a4f3c6156b00e171ff
Arif Laksonodhewo <arifldhewo234@gmail.com>
2026-06-17 17:31:50 +0700

    [Feature]: Create transactions table, and alter table transactions_ledger

--- BE/src/database/schemas.ts
+++ BE/src/database/schemas.ts
@@ -1,37 +1,52 @@
-import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp, bigint } from "drizzle-orm/pg-core";
+import { HttpMethod, TransactionLedgerStatuses } from "./schemas.types";
 
 export const webhooksTable = pgTable("webhooks", {
     ID: uuid("id").primaryKey().notNull().defaultRandom(),
     url: text("url").notNull(),
-    httpMethod: text("http_method", { enum: ["POST", "PUT", "PATCH"] })
-        .default("POST")
-        .notNull(),
+    httpMethod: text("http_method").$type<HttpMethod>().default(HttpMethod.POST).notNull(),
     keyHeader: text("key_header"),
     keyValue: text("key_value"),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().notNull(),
-    deletedAt: timestamp("deleted_at").defaultNow().notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true }),
+    updatedAt: timestamp("updated_at", { withTimezone: true }),
+    deletedAt: timestamp("deleted_at", { withTimezone: true }),
 });
 
 export const transactionCategories = pgTable("transaction_categories", {
     ID: uuid("id").primaryKey().notNull().defaultRandom(),
     name: text("name").notNull().unique(),
-    icon_url: text("icon_url"),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().notNull(),
-    deletedAt: timestamp("deleted_at").defaultNow().notNull(),
+    iconUrl: text("icon_url"),
+    createdAt: timestamp("created_at", { withTimezone: true }),
+    updatedAt: timestamp("updated_at", { withTimezone: true }),
+    deletedAt: timestamp("deleted_at", { withTimezone: true }),
 });
 
 export const transactionSubCategories = pgTable("transaction_sub_categories", {
     ID: uuid("id").primaryKey().notNull().defaultRandom(),
     name: text("name").notNull().unique(),
-    icon_url: text("icon_url"),
+    iconUrl: text("icon_url"),
     category_id: text("category_id").notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().notNull(),
-    deletedAt: timestamp("deleted_at").defaultNow().notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true }),
+    updatedAt: timestamp("updated_at", { withTimezone: true }),
+    deletedAt: timestamp("deleted_at", { withTimezone: true }),
 });
 
 export const transactionLedgers = pgTable("transaction_ledgers", {
     ID: uuid("id").primaryKey().notNull().defaultRandom(),
+    emailMessageID: text("email_message_id").unique().$type(),
+    bankSource: text("bank_source"),
+    type: text("type"),
+    category: text("category"),
+    acquirer: text("acquirer"),
+    status: text("status").$type<TransactionLedgerStatuses>(),
+    amount: bigint("amount", { mode: "number" }),
+    senderName: text("sender_name"),
+    senderAccount: text("sender_account"),
+    recipientName: text("recipientName"),
+    recipientAccount: text("recipientAccount"),
+    merchantLocation: text("merchant_location"),
+    remarks: text("remarks"),
+    transactionDate: timestamp("transaction_date", { withTimezone: true }),
+    referenceNumber: text("reference_number"),
+    createdAt: timestamp("created_at", { withTimezone: true }),
 });
---
+++ BE/src/database/schemas.types.ts
@@ -0,0 +1,13 @@
+export enum HttpMethod {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    PATCH = "PATCH",
+    DELETE = "DELETE",
+}
+
+export enum TransactionLedgerStatuses {
+    SUCCESS = "SUCCESS",
+    FAILED = "FAILED",
+    UNKNOWN = "UNKNOWN",
+}
